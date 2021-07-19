ac97618979483166820cd8860fec34ecaf014f03
tiloziegler <86304037+tiloziegler@users.noreply.github.com>
2021-07-19 15:26:53 +0200

    Update RunCommand.php

--- Command/RunCommand.php
+++ Command/RunCommand.php
@@ -24,7 +24,7 @@ use JMS\JobQueueBundle\Entity\Repository\JobManager;
 use JMS\JobQueueBundle\Event\NewOutputEvent;
 use JMS\JobQueueBundle\Event\StateChangeEvent;
 use JMS\JobQueueBundle\Exception\InvalidArgumentException;
-use Symfony\Bridge\Doctrine\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Registry;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -67,11 +67,11 @@ class RunCommand extends Command
     /** @var array */
     private $queueOptions;
 
-    public function __construct(ManagerRegistry $managerRegistry, JobManager $jobManager, EventDispatcherInterface $dispatcher, array $queueOptionsDefault, array $queueOptions)
+    public function __construct(Registry $registry, JobManager $jobManager, EventDispatcherInterface $dispatcher, array $queueOptionsDefault, array $queueOptions)
     {
         parent::__construct();
 
-        $this->registry = $managerRegistry;
+        $this->registry = $registry;
         $this->jobManager = $jobManager;
         $this->dispatcher = $dispatcher;
         $this->queueOptionsDefault = $queueOptionsDefault;
